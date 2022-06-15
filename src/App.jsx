3bbd50423a0078edec96e989e3e7e05662f49f58
Sonia Alvarez <soniaalvarez2911@gmail.com>
2022-06-14 23:24:26 -0300

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
+//@ts-check
+
 import logo from './canastita.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
+    <><NavBar />
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" typeof='click'/>
+        <img src={logo} className="App-logo" alt="logo" typeof='click' />
         <p>
           HOLA CODER!
         </p>
       </header>
-    </div>
+    </div></>
   );
 }  
 
